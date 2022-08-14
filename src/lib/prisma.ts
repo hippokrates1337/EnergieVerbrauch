de4cb9b4f846ffab06659384c8efc177763fd009
hippokrates1337 <j.weber_2003@web.de>
2022-08-14 08:28:03 +0000

    Made createdAt field default to now()

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,5 @@
+// Use 'import PrismaClient from "$lib/prisma";' to use the Prisma client
+
 import Prisma, * as PrismaAll from "@prisma/client";
 
 const PrismaClient = Prisma?.PrismaClient || PrismaAll?.PrismaClient;
