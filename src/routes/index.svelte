ba452a723adcd669b984b6825fb1fddaeb58b674
hippokrates1337 <j.weber_2003@web.de>
2022-08-14 08:57:16 +0000

    Vite HMR still not working, tried removing config

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Vergleiche Deinen Energieverbrauch</h1>
+
+<form action="" method="">
+    <button>Register</button>
+</form>
+
