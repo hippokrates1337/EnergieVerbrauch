a9522b7965970d4c1014eeb38d8e58d1a7ce417f
hippokrates1337 <j.weber_2003@web.de>
2022-08-16 20:34:33 +0000

    New user persisted in database

--- src/routes/register/index.json.ts
+++ src/routes/register/index.json.ts
@@ -1,9 +1,12 @@
 import type { RequestHandler } from "@sveltejs/kit";
 
 export const POST: RequestHandler = async (event) => {
-    const values = (await event.request.formData()).get("userName");
+    const userName = (await event.request.formData()).get("userName");
+    const email = (await event.request.formData()).get("email");
+    const password = (await event.request.formData()).get("password");
+
     return {
         status: 200,
-        body: values
+        body: {userName: userName, email: email, password: password}
     }
 }
