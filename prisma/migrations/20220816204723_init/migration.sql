c0966d84fadbf2c125db29d8288b03bafdc0e530
hippokrates1337 <j.weber_2003@web.de>
2022-08-16 20:47:55 +0000

    Recreated initial migration

--- prisma/migrations/20220816204723_init/migration.sql
+++ prisma/migrations/20220816204723_init/migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE "User" (
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("uid")
 );
