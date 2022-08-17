7a6eb354f126b375f45e81ab408343ff969eae7a
hippokrates1337 <j.weber_2003@web.de>
2022-08-17 13:11:04 +0000

    Added password encryption using bcrypt

--- src/routes/register/index.json.ts
+++ src/routes/register/index.json.ts
@@ -1,16 +1,29 @@
-import type { RequestHandler } from "@sveltejs/kit";
+import type { RequestEvent, RequestHandler } from "@sveltejs/kit";
 import PrismaClient from "$lib/prisma";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
+const saltRounds = 10;
 
-export const POST: RequestHandler = async (event) => {
+export const POST: RequestHandler = async (event: RequestEvent) => {
     const data = await event.request.formData();
 
-    await prisma.user.create({
-        data: {
-            name: data.get("userName") as string,
-            email: data.get("email") as string,
-            password: data.get("password") as string
+    await bcrypt.hash(data.get("password") as string, saltRounds, async (err, hash) => {
+        if(err) {
+            return {
+                status: 500,
+                errors: {
+                    password: "Unable to hash password"
+                }
+            };
+        } else {
+            await prisma.user.create({
+                data: {
+                    name: data.get("userName") as string,
+                    email: data.get("email") as string,
+                    password: hash as string
+                }
+            });
         }
     });
 
@@ -19,5 +32,5 @@ export const POST: RequestHandler = async (event) => {
         headers: {
             location: "/register"
         }
-    }
+    };
 }
