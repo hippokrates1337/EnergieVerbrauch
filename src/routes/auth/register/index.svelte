41996f959731a86b244dd9f81355e5388ceb8d15
hippokrates1337 <j.weber_2003@web.de>
2022-09-02 21:25:44 +0000

    Interim commit

--- src/routes/auth/register/index.svelte
+++ src/routes/auth/register/index.svelte
@@ -42,39 +42,52 @@
     <title>Als neuer Benutzer registrieren</title>
 </svelte:head>
 
-<div class="container border border-secondary p-3">
+<div class="container border border-secondary p-3 mt-5">
+    <h3 class="text-dark">Als neuer Benutzer anmelden</h3>
     <form on:submit|preventDefault={register} method="post" autocomplete="off">
         <div class="row mb-2">
             <div class="col">
-                <input placeholder="Benutzername" class="form-control" type="text" id="userName" name="userName" required aria-label="Specify your user name" />
+                <div class="form-floating">
+                    <input placeholder="Benutzername" class="form-control" type="text" id="userName" name="userName" required aria-label="Specify your user name" />
+                    <label for="userName">Benutzername</label>
+                </div>
             </div>
             <div class="col">
-                <input placeholder="E-Mail-Adresse" class="form-control" type="text" id="email" name="email" required aria-label="Provide an email address" />
+                <div class="form-floating">
+                    <input placeholder="E-Mail-Adresse" class="form-control" type="text" id="email" name="email" required aria-label="Provide an email address" />
+                    <label for="email">E-Mail-Adresse</label>
+                </div>
             </div>
         </div>
         <div class="row mb-2">
             <div class="col">
-                <input placeholder="Passwort" class="form-control" type="password" id="password" name="password" required aria-label="Select a password" />
+                <div class="form-floating">
+                    <input placeholder="Passwort" class="form-control" type="password" id="password" name="password" required aria-label="Select a password" />
+                    <label for="password">Passwort</label>
+                </div>
             </div>
             <div class="col">
-                <input placeholder="Passwort wiederholen" class="form-control" type="password" id="passwordRepeat" name="passwordRepeat" required aria-label="Repeat password" />
+                <div class="form-floating">
+                    <input placeholder="Passwort wiederholen" class="form-control" type="password" id="passwordRepeat" name="passwordRepeat" required aria-label="Repeat password" />
+                    <label for="passwordRepeat">Passwort wiederholen</label>
+                </div>
             </div>
         </div>
         <div class="row">
             <div class="col">
-                <button class="btn btn-secondary" type="submit" aria-label="Submit user registration">Registrieren</button>
+                <div class="float-end">
+                    <button class="btn btn-secondary" type="submit" aria-label="Submit user registration">Registrieren</button>
+                </div>
             </div>
         </div>
     </form>
-</div>
+    {#if error}
+        <p class="text-danger">{error}</p>
+    {/if}
 
-{#if error}
-<p>{error}</p>
-{/if}
-
-{#if success}
-<div>
-    <p>Erfolgreich als neuer Benutzer registriert.</p>
-    <p>Du kannst Dich nun <a href="/auth/login">einloggen</a>.</p>
+    {#if success}
+    <div>
+        <p class="text-success">Erfolgreich als neuer Benutzer registriert. Du kannst Dich nun <a href="/auth/login">einloggen</a>.</p>
+    </div>
+    {/if}
 </div>
-{/if}
