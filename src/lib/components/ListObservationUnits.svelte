41aba2eee0467a5cff9db0c70b58f677a6ca175d
hippokrates1337 <j.weber_2003@web.de>
2022-09-11 06:00:40 +0000

    Interim commit

--- src/lib/components/ListObservationUnits.svelte
+++ src/lib/components/ListObservationUnits.svelte
@@ -2,9 +2,23 @@
     export let obsUnits: ObservationUnit[];
 </script>
 
-<ul>
-    {#each obsUnits as unit}
-        <li>{unit.name}</li>
-    {/each}
-</ul>
+<div class="row">
+    <div class="col-4">
+        <div class="list-group list-group-light" id="listTab" role="tablist">
+            {#each obsUnits as unit, i}
+                <a class="list-group-item list-group-item-action px-3 border-0 {i == 0 ? 'active' : ''}" id="list{unit.name}List" data-mdb-toggle="list" href="#list{unit.name}" role="tab" aria-controls="list{unit.name}">{unit.name}</a>
+            {/each}
+        </div>
+    </div>
+    <div class="col-8">
+        <div class="tab-content" id="navTabContent">
+            {#each obsUnits as unit, i}
+                <div class="tab-pane fade {i == 0 ? 'active show' : ''}" id="list{unit.name}" role="tabpanel" aria-labelledby="list{unit.name}List">
+                    {unit.name}
+                </div>
+            {/each}
+        </div>
+    </div>
+</div>
+
 
--- src/routes/protected/manageObsUnits.svelte
+++ src/routes/protected/manageObsUnits.svelte
@@ -51,10 +51,21 @@
 <section class="vh-100">
     <div class="container h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
-            <h2>Verbraucher verwalten</h2>
+            <div class="col-lg-12 col-xl-11">
+                <div class="card text-black" style="border-radius: 25px; background-color: #19FFAF">
+                    <div class="card-body p-md-5">
+                        <div class="row justify-content-center">
+                            <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                                <h2>Verbraucher verwalten</h2>
 
-            <ListObservationUnits {obsUnits}/>
-            <AddObservationUnit on:add={e => addUnit(e.detail)} {addUnitError}/>
+                                <ListObservationUnits {obsUnits}/>
+                                <hr style="border-top: 3px double #8c8b8b">
+                                <AddObservationUnit on:add={e => addUnit(e.detail)} {addUnitError}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
