091af86be23a54f1ecf82110db844faecf15d14c
hippokrates1337 <j.weber_2003@web.de>
2022-09-18 16:29:25 +0000

    Interim commit

--- src/lib/components/ListObservationUnits.svelte
+++ src/lib/components/ListObservationUnits.svelte
@@ -1,21 +1,36 @@
 <script lang="ts">
     export let obsUnits: ObservationUnit[];
+    export let observations: Observation[];
+    let showEdit:boolean = false;
+
+    const toggleEdit = () => {
+        showEdit == !showEdit;
+        alert("This worked");
+    }
+
 </script>
 
-<div class="row">
+<div class="row bg-light p-3">
     <div class="col-4">
         <div class="list-group list-group-light" id="listTab" role="tablist">
             {#each obsUnits as unit, i}
-                <a class="list-group-item list-group-item-action px-3 border-0 {i == 0 ? 'active' : ''}" id="list{unit.name}List" data-bs-toggle="list" href="#list{unit.name}" role="tab" aria-controls="list{unit.name}">{unit.name}</a>
+                <a class="list-group-item list-group-item-action border-0 {i == 0 ? 'active' : ''}" id="list{i}Link" data-bs-toggle="list" href="#list{i}" role="tab" aria-controls="list{i}">{unit.name}</a>
             {/each}
         </div>
     </div>
     <div class="col-8">
         <div class="tab-content" id="navTabContent">
             {#each obsUnits as unit, i}
-                <div class="tab-pane fade {i == 0 ? 'active show' : ''} px-3 border-0 bg-light" id="list{unit.name}" role="tabpanel" aria-labelledby="list{unit.name}List">
-                    <b>Name:</b> {unit.name}<br>
-                    <b>Hinzugefügt am:</b> {new Date(unit.createdAt).toDateString()} 
+                <div class="tab-pane fade {i == 0 ? 'active show' : ''} border-0 bg-light" id="list{i}" role="tabpanel" aria-labelledby="list{i}">
+                    <b>Name:</b> {unit.name}
+                    <i class="fa fa-pencil" on:click={toggleEdit}></i>
+                    {#if showEdit}
+                        <input type="text" />
+                        <button type="submit">Ändern</button>
+                    {/if}
+                    <br>
+                    <b>Hinzugefügt am:</b> {new Date(unit.createdAt).toDateString()}
+                    <b>Hinterlegte Verbrauchswerte:</b> {observations.filter(e => e.obsUnit == unit.name).length}
                 </div>
             {/each}
         </div>
--- src/routes/protected/manageObsUnits.svelte
+++ src/routes/protected/manageObsUnits.svelte
@@ -16,10 +16,18 @@
             units = await res.json();
         }
 
+        // Load user's observations (to be passed into props)
+        res = await fetch("/protected/observations");
+        let obs;
+        if(res.ok) {
+            obs = await res.json();
+        }
+
         return {
             status: 200,
             props: {
-                obsUnits: units.data
+                obsUnits: units.data,
+                observations: obs.data
             }
         };
     }
@@ -31,6 +39,7 @@
     import AddObservationUnit from "$lib/components/AddObservationUnit.svelte";
 
     export let obsUnits: ObservationUnit[];
+    export let observations: Observation[];
     let addUnitError = "";
 
     const addUnit = async (formElement: HTMLFormElement) => {
@@ -55,10 +64,10 @@
                 <div class="card text-black" style="border-radius: 25px; background-color: #19FFAF">
                     <div class="card-body p-md-5">
                         <div class="row justify-content-center">
-                            <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                            <div class="col-md-10 col-lg-8 col-xl-6 order-2 order-lg-1">
                                 <h2>Verbraucher verwalten</h2>
 
-                                <ListObservationUnits {obsUnits}/>
+                                <ListObservationUnits {obsUnits} {observations}/>
                                 <hr style="border-top: 3px double #8c8b8b">
                                 <AddObservationUnit on:add={e => addUnit(e.detail)} {addUnitError}/>
                             </div>
