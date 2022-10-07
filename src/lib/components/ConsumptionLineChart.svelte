ee7abf9f636d2929416ae107e51fbf63aace131c
hippokrates1337 <j.weber_2003@web.de>
2022-10-07 21:14:21 +0000

    Started line chart component

---
+++ src/lib/components/ConsumptionLineChart.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let obsUnits: ObservationUnit[];
+    export let observations: Observation[];
+
+    // Determine first and last date to show
+    let startDate = observations.map(obs => obs.startDate).sort((a, b) => new Date(a) - new Date(b))[0];
+    let endDate = observations.map(obs => obs.endDate).sort((a, b) => new Date(b) - new Date(a))[0];
+    let minValue = observations.map(obs => obs.value).sort((a, b) => a - b)[0];
+    let maxValue = observations.map(obs => obs.value).sort((a, b) => b - a)[0];
+</script>
+
+<p>
+    Start date: {startDate} <br>
+    End date: {endDate} <br>
+    Min value: {minValue} <br>
+    Max value: {maxValue}
+</p>
--- src/routes/protected/index.svelte
+++ src/routes/protected/index.svelte
@@ -9,8 +9,35 @@
             };
         }
 
+        // Load user's observation units (to be passed into props)
+        let res = await fetch("/protected/obsunits");
+        let units;
+        if(res.ok) {
+            units = await res.json();
+        }
+
+        // Load user's observations (to be passed into props)
+        res = await fetch("/protected/observations");
+        let obs;
+        if(res.ok) {
+            obs = await res.json();
+        }
+
         return {
-            status: 200
+            status: 200,
+            props: {
+                obsUnits: units.data,
+                observations: obs.data
+            }
         };
     }
 </script>
+
+<script lang="ts">
+    import ConsumptionLineChart from "$lib/components/ConsumptionLineChart.svelte";
+
+    export let obsUnits: ObservationUnit[];
+    export let observations: Observation[];
+</script>
+
+<ConsumptionLineChart {obsUnits} {observations} />
